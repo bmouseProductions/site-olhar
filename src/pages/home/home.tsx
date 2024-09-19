ccf87760eadfcce77b5f71176ab82ee3c79c5f55
bmouseProductions <designers@bmouseproductions.com>
2024-09-19 10:44:17 -0300

    finished

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,7 +9,6 @@ export default function Home() {
 
     return (
         <>
-            {}
             <SectionBanner />
             <SectionEspecialidades />
             <SectionQuemSomos />
--- src/pages/home/sections/section-banner.tsx
+++ src/pages/home/sections/section-banner.tsx
@@ -1,7 +1,8 @@
 import { NavLink } from 'react-router-dom'
 import ModalVideo from '../../../components/modal-video-home'
 import backgroundBanner from '../../../assets/images/background-banner.svg'
-import doutorasBanner from '../../../assets/images/doutoras.svg'
+import doutorasDesktop from '../../../assets/images/doutoras.svg'
+import doutorasMobile from '../../../assets/images/doutoras-mobile.svg'
 import itestocopioIcon from '../../../assets/images/icons/itestocopio.svg'
 import itestocopioAzulIcon from '../../../assets/images/icons/itestocopio-azul.svg'
 
@@ -10,49 +11,69 @@ export default function SectionBanner() {
     const widthScreen = window.innerWidth
 
     return (
-        <section className="container mx-auto py-10 px-5 md:px-10 lg:px-14 xl:px-16">
+        <section className="">
             {
                 widthScreen >= 768 ? 
                 //BANNER DESKTOP
-                <div className='relative'>
-                    <div className='relative flex justify-center'>
-                        <img 
-                            src={backgroundBanner}
-                            alt="banner  com palavras desfocadas" 
-                            className="w-full "
-                        />
+                <div className='container mx-auto py-10 md:px-10 lg:px-14 xl:px-16'>
+                    <div className='relative '>
+                        <div className='relative flex justify-center'>
+                            <img 
+                                src={backgroundBanner}
+                                alt="banner  com palavras desfocadas" 
+                                className="w-full "
+                            />
 
-                        <img 
-                            src={doutorasBanner}
-                            alt="banner  com palavras desfocadas" 
-                            className="w-1/2 absolute z-10 bottom-0 ml-auto "
-                        />
+                            <img 
+                                src={doutorasDesktop}
+                                alt="banner  com doutoras Marina e Cristina" 
+                                className="w-1/2 absolute z-10 bottom-0 ml-auto "
+                            />
 
-                        <h1 className='mt-5 absolute text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white '>
-                            seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
-                        </h1>
-                    </div>
+                            <h1 className='mt-5 absolute text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white '>
+                                seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+                            </h1>
+                        </div>
 
-                    <a
-                        href=''
-                        target='_blank'
-                        className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
-                    >
-                        Faça seu agendamento
-                    </a>
+                        <a
+                            href=''
+                            target='_blank'
+                            className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
+                        >
+                            Faça seu agendamento
+                        </a>
 
-                    
-                    <ModalVideo children= "Assista ao video" />
-                </div> 
+                        
+                        <ModalVideo children= "Assista ao video" />
+                    </div> 
+                </div>
                 :
                 //BANNER MOBILE
-                <div>
+                <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa]  via-[#96cab1] to-[#3e415c] '>
+                    <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
+                        seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+                    </h1>
 
+                    <img 
+                        src={doutorasMobile} 
+                        alt="banner  com doutoras Marina e Cristina"
+                        className='w-full'
+                    />
+
+                    <div className='absolute -bottom-5 w-full flex justify-center'>
+                        <a
+                            href=''
+                            target='_blank'
+                            className=' px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
+                        >
+                            Faça seu agendamento
+                        </a>
+                    </div>
                 </div>
             }
             
 
-            <div className='mt-7 grid grid-cols-1 md:grid-cols-2 gap-5'>
+            <div className='container mx-auto px-5 py-10 md:px-10 lg:px-14 xl:px-16 mt-7 grid grid-cols-1 md:grid-cols-2 gap-5'>
                 <NavLink 
                     to=""
                     className='p-4 space-y-3 bg-white rounded-2xl shadow-lg hover:shadow-sm transition-all'
