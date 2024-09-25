9d302b1315c3abb71ec5825ba17db4ac46a551f0
bmouseProductions <designers@bmouseproductions.com>
2024-09-25 13:45:26 -0300

    alteração loading

--- src/pages/home/sections/section-banner.tsx
+++ src/pages/home/sections/section-banner.tsx
@@ -1,58 +1,56 @@
-import { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-
-import { useLoading } from '../../../contexts/loading-provider'
-
-import ModalVideo from '../../../components/modal-video-home'
-
-import backgroundBanner from '../../../assets/images/background-banner.svg'
-import doutorasDesktop from '../../../assets/images/doutoras.svg'
-import doutorasMobile from '../../../assets/images/doutoras-mobile.svg'
-import itestocopioIcon from '../../../assets/images/icons/itestocopio.svg'
-import itestocopioAzulIcon from '../../../assets/images/icons/itestocopio-azul.svg'
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useLoading } from '../../../contexts/loading-provider';
+import ModalVideo from '../../../components/modal-video-home';
+import backgroundBanner from '../../../assets/images/background-banner.svg';
+import doutorasDesktop from '../../../assets/images/doutoras.svg';
+import doutorasMobile from '../../../assets/images/doutoras-mobile.svg';
+import itestocopioIcon from '../../../assets/images/icons/itestocopio.svg';
+import itestocopioAzulIcon from '../../../assets/images/icons/itestocopio-azul.svg';
 
 export default function SectionBanner() {
     const { setLoading } = useLoading();
+    const [loadedImages, setLoadedImages] = useState(0);
+    const totalImages = 3; // Update this based on the number of images
 
     const handleImageLoad = () => {
-        setLoading(false); 
-    };
-
-    const handleImageError = () => {
-        setLoading(false);
+        setLoadedImages((prev) => prev + 1);
     };
 
     useEffect(() => {
-        setLoading(true); 
+        setLoading(true);
     }, [setLoading]);
 
-    const widthScreen = window.innerWidth
+    useEffect(() => {
+        if (loadedImages === totalImages) {
+            setLoading(false);
+        }
+    }, [loadedImages, setLoading]);
+
+    const widthScreen = window.innerWidth;
 
     return (
-        <section className="">
-            {
-                widthScreen >= 768 ? 
-                //BANNER DESKTOP
+        <section>
+            {widthScreen >= 768 ? 
+                // BANNER DESKTOP
                 <div className='container mx-auto py-10 xl:py-14 md:px-10 lg:px-14 xl:px-16'>
-                    <div className='relative '>
+                    <div className='relative'>
                         <div className='relative flex justify-center'>
                             <img 
                                 src={backgroundBanner}
-                                alt="banner  com palavras desfocadas" 
+                                alt="banner com palavras desfocadas" 
                                 className="w-full shadow rounded-3xl"
                                 onLoad={handleImageLoad}
-                                onError={handleImageError}
                             />
 
                             <img 
                                 src={doutorasDesktop}
-                                alt="banner  com doutoras Marina e Cristina" 
-                                className="w-[42%] absolute z-10 bottom-0 ml-auto "
+                                alt="banner com doutoras Marina e Cristina" 
+                                className="w-[42%] absolute z-10 bottom-0 ml-auto"
                                 onLoad={handleImageLoad}
-                                onError={handleImageError}
                             />
 
-                            <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white '>
+                            <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white'>
                                 seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
                             </h1>
                         </div>
@@ -65,48 +63,45 @@ export default function SectionBanner() {
                             Faça seu agendamento
                         </a>
 
-                        
-                        <ModalVideo children= "Assista ao video" />
-                    </div> 
+                        <ModalVideo children="Assista ao video" />
+                    </div>
                 </div>
                 :
-                //BANNER MOBILE
-                <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa]  via-[#96cab1] to-[#3e415c] '>
+                // BANNER MOBILE
+                <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa] via-[#96cab1] to-[#3e415c]'>
                     <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
                         seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
                     </h1>
 
                     <img 
                         src={doutorasMobile} 
-                        alt="banner  com doutoras Marina e Cristina"
+                        alt="banner com doutoras Marina e Cristina"
                         className='w-full'
                         onLoad={handleImageLoad}
-                        onError={handleImageError}
                     />
 
                     <div className='absolute -bottom-5 w-full flex justify-center'>
                         <a
                             href=''
                             target='_blank'
-                            className=' px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
+                            className='px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
                         >
                             Faça seu agendamento
                         </a>
                     </div>
                 </div>
             }
-            
 
             <div className='container mx-auto py-10 md:pb-14 px-5 md:px-10 lg:px-14 xl:px-16 grid grid-cols-1 md:grid-cols-2 gap-5'>
                 <NavLink 
                     to="/exames-cirugias#exames"
                     className='p-4 space-y-3 text-[#3d405b] bg-white rounded-2xl shadow-lg hover:shadow-sm transition-all'
                 >
-                    <div className='w-fit p-3 bg-[#3d405b] rounded-full '>
+                    <div className='w-fit p-3 bg-[#3d405b] rounded-full'>
                         <img 
                             src={itestocopioIcon} 
                             alt="icon itestoscópio"
-                            className='!text-red-700 '
+                            className='!text-red-700'
                         />
                     </div>
 
@@ -127,14 +122,14 @@ export default function SectionBanner() {
                     to="/exames-cirugias#cirurgias"
                     className='p-4 space-y-3 text-white bg-[#3d405b] rounded-2xl shadow-lg hover:shadow-sm transition-all'
                 >
-                    <div className='w-fit p-3 bg-white rounded-full '>
+                    <div className='w-fit p-3 bg-white rounded-full'>
                         <img 
                             src={itestocopioAzulIcon} 
                             alt="icon itestoscópio"
                         />
                     </div>
 
-                    <h3 className='text-2xl font-semibold '>Cirurgias</h3>
+                    <h3 className='text-2xl font-semibold'>Cirurgias</h3>
 
                     <p>
                         Restaurar a sua visão é a nossa prioridade! Na nossa clínica, oferecemos cirurgias oculares realizadas
@@ -148,5 +143,5 @@ export default function SectionBanner() {
                 </NavLink>
             </div>
         </section>
-    )
+    );
 }
