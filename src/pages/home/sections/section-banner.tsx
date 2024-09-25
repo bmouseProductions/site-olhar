8b2c72a4ea4e84de314bbf129210ee96586ea8e6
bmouseProductions <designers@bmouseproductions.com>
2024-09-25 15:40:37 -0300

    alteração loading

--- src/pages/home/sections/section-banner.tsx
+++ src/pages/home/sections/section-banner.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import ModalVideo from '../../../components/modal-video-home';
 import backgroundBanner from '../../../assets/images/background-banner.svg';
@@ -15,67 +16,70 @@ export default function SectionBanner() {
     return (
         <section>
             {widthScreen >= 768 ? 
-    // BANNER DESKTOP
-    <div className='container mx-auto py-10 xl:py-14 md:px-10 lg:px-14 xl:px-16'>
-        <div className='relative'>
-            <div className='relative flex justify-center'>
-                <LazyLoadImage 
-                    src={backgroundBanner}
-                    alt="banner com palavras desfocadas" 
-                    effect="blur"
-                    className="w-full shadow rounded-3xl"
-                />
+                // BANNER DESKTOP
+                <div className='container mx-auto py-10 xl:py-14 md:px-10 lg:px-14 xl:px-16'>
+                    <div className='relative'>
+                        <div className='relative flex justify-center'>
+                            <LazyLoadImage 
+                                src={backgroundBanner}
+                                alt="banner com palavras desfocadas" 
+                                className="w-full shadow rounded-3xl"
+                            />
 
-                <LazyLoadImage 
-                    src={doutorasDesktop}
-                    alt="banner com doutoras Marina e Cristina" 
-                    effect="blur"
-                    className="w-[42%] absolute z-10 bottom-0 ml-auto"
-                />
+                            <LazyLoadImage 
+                                src={doutorasDesktop}
+                                alt="banner com doutoras Marina e Cristina" 
+                                effect="blur"
+                                wrapperProps={{
+                                    style: {transitionDelay: "0.3s", position: "absolute", zIndex: "10", bottom:"0", width: "100%", display: "flex", justifyContent: "center"},
+                                }}
+                                className="!w-[42%] !absolute !z-10 !bottom-0 !ml-auto"
+                            />
 
-                <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white'>
-                    seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
-                </h1>
-            </div>
+                            <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white'>
+                                seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+                            </h1>
+                        </div>
 
-            <a
-                href=''
-                target='_blank'
-                rel='noopener noreferrer'
-                className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
-            >
-                Faça seu agendamento
-            </a>
+                        <a
+                            href=''
+                            target='_blank'
+                            className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
+                        >
+                            Faça seu agendamento
+                        </a>
 
-            <ModalVideo children="Assista ao video" />
-        </div>
-    </div>
-    :
-    // BANNER MOBILE
-    <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa] via-[#96cab1] to-[#3e415c]'>
-        <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
-            seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
-        </h1>
+                        <ModalVideo children="Assista ao video" />
+                    </div>
+                </div>
+                :
+                // BANNER MOBILE
+                <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa] via-[#96cab1] to-[#3e415c]'>
+                    <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
+                        seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+                    </h1>
 
-        <LazyLoadImage 
-            src={doutorasMobile} 
-            alt="banner com doutoras Marina e Cristina"
-            effect="blur"
-            className='w-full'
-        />
+                    <LazyLoadImage 
+                        src={doutorasMobile} 
+                        alt="banner com doutoras Marina e Cristina"
+                        effect="blur"
+                        wrapperProps={{
+                            style: {transitionDelay: "0.3s", width: "100%"},
+                        }}
+                        className='w-full'
+                    />
 
-        <div className='absolute -bottom-5 w-full flex justify-center'>
-            <a
-                href=''
-                target='_blank'
-                rel='noopener noreferrer'
-                className='px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
-            >
-                Faça seu agendamento
-            </a>
-        </div>
-    </div>
-}
+                    <div className='absolute -bottom-5 w-full flex justify-center'>
+                        <a
+                            href=''
+                            target='_blank'
+                            className='px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
+                        >
+                            Faça seu agendamento
+                        </a>
+                    </div>
+                </div>
+            }
 
             <div className='container mx-auto py-10 md:pb-14 px-5 md:px-10 lg:px-14 xl:px-16 grid grid-cols-1 md:grid-cols-2 gap-5'>
                 <NavLink 
