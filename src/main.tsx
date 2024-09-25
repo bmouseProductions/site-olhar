1c366c1831ea060d7b0b173e51703915ab762cae
bmouseProductions <designers@bmouseproductions.com>
2024-09-25 14:38:48 -0300

    alteração loading

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router} from "react-router-dom";
 
 import App from './app';
-import { LoadingProvider } from './contexts/loading-provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <LoadingProvider>
-        <App />
-      </LoadingProvider>
+      <App />
     </Router>
   </React.StrictMode>,
 )
--- src/pages/home/sections/section-banner.tsx
+++ src/pages/home/sections/section-banner.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useLoading } from '../../../contexts/loading-provider';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 import ModalVideo from '../../../components/modal-video-home';
 import backgroundBanner from '../../../assets/images/background-banner.svg';
 import doutorasDesktop from '../../../assets/images/doutoras.svg';
@@ -9,88 +9,73 @@ import itestocopioIcon from '../../../assets/images/icons/itestocopio.svg';
 import itestocopioAzulIcon from '../../../assets/images/icons/itestocopio-azul.svg';
 
 export default function SectionBanner() {
-    const { setLoading } = useLoading();
-    const [loadedImages, setLoadedImages] = useState(0);
-    const totalImages = 3; // Update this based on the number of images
-
-    const handleImageLoad = () => {
-        setLoadedImages((prev) => prev + 1);
-    };
-
-    useEffect(() => {
-        setLoading(true);
-    }, [setLoading]);
-
-    useEffect(() => {
-        if (loadedImages === totalImages) {
-            setLoading(false);
-        }
-    }, [loadedImages, setLoading]);
 
     const widthScreen = window.innerWidth;
 
     return (
         <section>
             {widthScreen >= 768 ? 
-                // BANNER DESKTOP
-                <div className='container mx-auto py-10 xl:py-14 md:px-10 lg:px-14 xl:px-16'>
-                    <div className='relative'>
-                        <div className='relative flex justify-center'>
-                            <img 
-                                src={backgroundBanner}
-                                alt="banner com palavras desfocadas" 
-                                className="w-full shadow rounded-3xl"
-                                onLoad={handleImageLoad}
-                            />
+    // BANNER DESKTOP
+    <div className='container mx-auto py-10 xl:py-14 md:px-10 lg:px-14 xl:px-16'>
+        <div className='relative'>
+            <div className='relative flex justify-center'>
+                <LazyLoadImage 
+                    src={backgroundBanner}
+                    alt="banner com palavras desfocadas" 
+                    effect="blur"
+                    className="w-full shadow rounded-3xl"
+                />
 
-                            <img 
-                                src={doutorasDesktop}
-                                alt="banner com doutoras Marina e Cristina" 
-                                className="w-[42%] absolute z-10 bottom-0 ml-auto"
-                                onLoad={handleImageLoad}
-                            />
+                <LazyLoadImage 
+                    src={doutorasDesktop}
+                    alt="banner com doutoras Marina e Cristina" 
+                    effect="blur"
+                    className="w-[42%] absolute z-10 bottom-0 ml-auto"
+                />
 
-                            <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white'>
-                                seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
-                            </h1>
-                        </div>
+                <h1 className='mt-5 absolute text-6xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl uppercase text-white'>
+                    seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+                </h1>
+            </div>
 
-                        <a
-                            href=''
-                            target='_blank'
-                            className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
-                        >
-                            Faça seu agendamento
-                        </a>
+            <a
+                href=''
+                target='_blank'
+                rel='noopener noreferrer'
+                className='absolute z-20 left-5 lg:left-10 bottom-5 lg:bottom-10 px-5 pt-2 pb-3 font-semibold lg:text-lg uppercase text-black bg-white rounded-3xl shadow'
+            >
+                Faça seu agendamento
+            </a>
 
-                        <ModalVideo children="Assista ao video" />
-                    </div>
-                </div>
-                :
-                // BANNER MOBILE
-                <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa] via-[#96cab1] to-[#3e415c]'>
-                    <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
-                        seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
-                    </h1>
+            <ModalVideo children="Assista ao video" />
+        </div>
+    </div>
+    :
+    // BANNER MOBILE
+    <div className='relative pt-10 bg-gradient-to-b from-[#f5f6fa] via-[#96cab1] to-[#3e415c]'>
+        <h1 className='mt-5 px-5 text-5xl uppercase text-white text-center'>
+            seu <span className='font-bold'>olhar</span> é nosso <span className='font-bold'>foco</span>
+        </h1>
 
-                    <img 
-                        src={doutorasMobile} 
-                        alt="banner com doutoras Marina e Cristina"
-                        className='w-full'
-                        onLoad={handleImageLoad}
-                    />
+        <LazyLoadImage 
+            src={doutorasMobile} 
+            alt="banner com doutoras Marina e Cristina"
+            effect="blur"
+            className='w-full'
+        />
 
-                    <div className='absolute -bottom-5 w-full flex justify-center'>
-                        <a
-                            href=''
-                            target='_blank'
-                            className='px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
-                        >
-                            Faça seu agendamento
-                        </a>
-                    </div>
-                </div>
-            }
+        <div className='absolute -bottom-5 w-full flex justify-center'>
+            <a
+                href=''
+                target='_blank'
+                rel='noopener noreferrer'
+                className='px-5 pt-2 pb-2.5 font-semibold lg:text-lg uppercase text-white bg-[#160f29] rounded-3xl shadow'
+            >
+                Faça seu agendamento
+            </a>
+        </div>
+    </div>
+}
 
             <div className='container mx-auto py-10 md:pb-14 px-5 md:px-10 lg:px-14 xl:px-16 grid grid-cols-1 md:grid-cols-2 gap-5'>
                 <NavLink 
